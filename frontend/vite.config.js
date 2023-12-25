5babb1ab661df3d2ca5173b3d9eed6f9205cf382
Anant <akuj1sep@gmail.com>
2023-12-25 20:16:13 +0530

    base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set your base URL here
+  // Other configurations...
 })
