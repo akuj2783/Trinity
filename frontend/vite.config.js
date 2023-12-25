47665d91a8aa8cd96230b2185aa5c529054a17fa
Anant <akuj1sep@gmail.com>
2023-12-26 02:53:43 +0530

    deploy work flow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://trinityinfonet.onrender.com/', // Set your base URL here
+  base:'/frontend/', // Set your base URL here
   // Other configurations...
 })
